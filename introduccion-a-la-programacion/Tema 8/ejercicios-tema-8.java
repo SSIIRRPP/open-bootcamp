67cc84b8c6d7dd11ab0e09d39e767768e3f24cb1
SSIIRRPP <69328047+SSIIRRPP@users.noreply.github.com>
2022-09-12 04:31:03 +0200

    Introducción a la programación: ejercicios tema 8

--- introduccion-a-la-programacion/Tema 8/ejercicios-tema-8.java
+++ introduccion-a-la-programacion/Tema 8/ejercicios-tema-8.java
@@ -1,1 +1,51 @@
+public class Main {
+    public static void main(String args[]) {
+        Persona persona = new Persona();
+        
+        persona.setEdad(25);
+        persona.setNombre("Pascual");
+        persona.setTelefono(123456789);
+        
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+        
+    }
+}
 
+class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+    
+    public Persona() {
+        this.edad = 0;
+        this.nombre = "";
+        this.telefono = 0;
+    }
+    
+    public int getEdad () {
+        return this.edad;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public int getTelefono () {
+        return this.telefono;
+    }
+    
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    
+}
